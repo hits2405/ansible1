85a34e877547e635514bbcd239c307a3100e6c14
hits2405 <108691585+hits2405@users.noreply.github.com>
2024-07-03 20:21:09 +0300

    Create ansible.sh

---
+++ ansible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Install
+sudo apt -y update
+sudo apt -y install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt -y install ansible
+mkdir ansible
+cd ansible
