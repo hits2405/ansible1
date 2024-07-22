4d6e43520f847ff8008084416fcbbbe8cc2a6954
hits2405 <108691585+hits2405@users.noreply.github.com>
2024-07-22 13:44:34 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -5,6 +5,6 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt -y install ansible
 cd ansible
-sudo cp /home/mat/ansible1/ansible.cfg /home/mat/ansible/ansible.cfg
-sudo cp /home/mat/ansible1/hosts /home/mat/ansible/hosts
+sudo mv /home/mat/ansible1/ansible.cfg /home/mat/ansible/ansible.cfg
+sudo mv /home/mat/ansible1/hosts /home/mat/ansible/hosts
 rm -Rfv /home/ansible
